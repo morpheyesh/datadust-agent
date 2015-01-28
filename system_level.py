391c59d53fc1994b6c0ab0e8817cbb84c20fe088
Yeshwanth <morpheyesh@gmail.com>
2015-01-28 19:44:16 +0530

    cpustats

--- system_level.py
+++ system_level.py
@@ -1,8 +1,9 @@
 import os
 import re
-
+import string
 import subprocess
 import platform
+import sys
 
 current_python = platform.python_version_tuple()
 
@@ -11,8 +12,6 @@ class system_level:
 
  def __init__(self): #Explicit is better than implicit 
    print "Done Init-ing!"  
-   
-   
    
    
    
@@ -31,10 +30,96 @@ class system_level:
    into_tuple = command_exec.split('\n')
    into_tuple.pop(0)                  
 
-   for single in into_tuple:
+   for singleStat in into_tuple:
       
-        single = single.split()
+        singleStat = singleStat.split()
         
-        disk_usage.append(single)
+        disk_usage.append(singleStat)
 
-   return disk_usage
+   return disk_usage  
+
+ def CPU_Stats(self):
+     
+    stats = {}
+          
+    if sys.platform == 'linux2':
+       print "Good to go!"
+       
+       itemR= re.compile(r'.*?\s+(\d+)[\s+]?')
+       valueR = re.compile(r'\d+\.\d+')  
+       headR = re.compile(r'.*?([%][a-zA-Z0-9]+)[\s+]?') 
+       proc = None
+       
+       try:
+          proc = subprocess.Popen(['mpstat', '-P', 'ALL', '1', '1'], stdout=subprocess.PIPE)
+          
+          mpstat = proc.communicate()[0]
+          print "----"
+          print mpstat
+          if int(current_python[1]) >= 6:
+              try:
+                  proc.kill()
+              except:
+                  print "proc is dead already! "
+          
+              mpstat = mpstat.split('\n')
+              mpstat.pop(1)      
+              head = mpstat[1]
+              headName = re.findall(headR, head)
+              
+              #take each line out
+               
+              for singleLine in range (2, len(mpstat)):
+                  singleIndex =  mpstat[singleLine]
+                  
+                  if not singleIndex:
+                      break
+                  
+                  device = re.match(itemR, singleIndex)
+                 
+                  if string.find(singleIndex, 'all') is not -1:
+                      ndevice = 'ALL'
+                  elif device is not None:
+                      ndevice = 'CPU%s' % device.groups()[0]
+                      
+                  values = re.findall(valueR, singleIndex.replace(',','.'))
+                  
+                  stats[ndevice] = {}
+                  
+                  for headerIndex in range(0, len(headName)):
+                      headNames = headName[headerIndex]
+                      stats[ndevice][headNames] = values[headerIndex]
+              
+
+       except OSError:
+          return false
+      
+    else:
+        print 'Different platform!'
+        return False
+
+    print 'Done!'
+    print stats
+    return stats
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from system_level import system_level
 
 a = system_level()
 
-z = a.Disk_Util()
-
-print z
+#z = a.Disk_Util()
+x = a.CPU_Stats()
+#print z
+print x
 print "GOtcha!"
